c63764ae6cb88bf4e399a1859a88cd1b52741830
Rostyslav Svistula <rostyslav.svistula@student.tuke.sk>
2024-03-19 19:34:52 +0100

    initial commit

---
+++ src/test/java/sk/posam/moneymate/MoneymateApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.posam.moneymate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneymateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
